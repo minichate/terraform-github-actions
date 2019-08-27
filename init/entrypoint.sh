2106c56a7c14a53e9d83a2b1b71321f1e52f14df
Christopher Troup <ctroup@freshbooks.com>
2019-08-27 16:39:07 -0400

    Hardcode github key

--- init/entrypoint.sh
+++ init/entrypoint.sh
@@ -19,6 +19,10 @@ fi
 if [[ ! -z "$GITHUB_DEPLOY_PRIVATE_KEY" ]]; then
   mkdir -p ${HOME}/.ssh
   ssh-keyscan -t rsa github.com > ${HOME}/.ssh/known_hosts
+  cat >> ${HOME}/.ssh/known_hosts << EOF
+# github.com:22 SSH-2.0-babeld-216c4091
+github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
+EOF
   chmod 644 ${HOME}/.ssh/known_hosts
 
   cat > ${HOME}/.ssh/config << EOF
